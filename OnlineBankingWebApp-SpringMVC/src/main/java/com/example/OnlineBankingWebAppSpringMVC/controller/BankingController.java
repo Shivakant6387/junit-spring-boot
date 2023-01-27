a316fb6c44925b27f42d0a29b5ad0b230ae1a2d4
Shivakant6387 <shivakant5875@gmail.com>
2023-01-27 17:04:24 +0530

    added transcation entity and dao and controller

---
+++ OnlineBankingWebApp-SpringMVC/src/main/java/com/example/OnlineBankingWebAppSpringMVC/controller/BankingController.java
@@ -0,0 +1,2 @@
+package com.example.OnlineBankingWebAppSpringMVC.controller;public class BankingController {
+}
---
+++ OnlineBankingWebApp-SpringMVC/src/main/java/com/example/OnlineBankingWebAppSpringMVC/entity/Transcation.java
@@ -0,0 +1,4 @@
+package com.example.OnlineBankingWebAppSpringMVC.entity;
+
+public class Transcation {
+}
