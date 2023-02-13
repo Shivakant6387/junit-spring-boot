e70ec43c1d48df09add3c837cce19ca8f87d0fc9
Shivakant6387 <shivakant5875@gmail.com>
2023-02-13 15:27:24 +0530

    added eureka client

--- employee-service-2-new/src/main/java/com/example/employeeservices/feignClient/AddressClient.java
+++ employee-service-2-new/src/main/java/com/example/employeeservices/feignClient/AddressClient.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "address-service",url = "http://localhost:19191",path = "/address-app/api/")
+
 public interface AddressClient {
     //localhost:19191/address-app/api/address/1
     //@GetMapping("/address/{id}")
